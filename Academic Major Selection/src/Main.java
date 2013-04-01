20529c0dfc02282f081d02267ec2e4d277d3f34b
James Collins <deafro5@hotmail.com>
2013-04-01 12:57:11 -0400

    GUI Java Files

---
+++ Academic Major Selection/src/Main.java
@@ -0,0 +1,15 @@
+import GUI.*; // imports GUI package
+
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Student myLogin = new Student();
+		//Login myLogin = new Login();
+	}
+
+}
